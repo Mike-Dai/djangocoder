a3e007bcd9133bb3a1ea2006821a1fabc004da78
Mike-Dai <mrmikedai@outlook.com>
2019-02-19 11:06:49 +0800

    Add sidebar through templatetags

---
+++ blog/templatetags/blog_tags.py
@@ -0,0 +1,12 @@
+from ..models import Post
+from django import template
+
+register = template.Library()
+
+@register.simple_tag
+def get_recent_posts(num=5):
+	return Post.objects.all().order_by('-created_date')[:num]
+
+@register.simple_tag
+def archives():
+	return Post.objects.dates('created_date','month',order='DESC')
